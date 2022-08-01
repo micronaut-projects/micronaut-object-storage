60313f47896dbb43d2d02deb5c4b179da32711f0
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2022-08-01 12:32:06 +0200

    Gradle 7.5, cleanup version catalog, fix bom build

--- object-storage-bom/build.gradle.kts
+++ object-storage-bom/build.gradle.kts
@@ -2,3 +2,11 @@ plugins {
     id("io.micronaut.build.internal.objectstorage-base")
     id("io.micronaut.build.internal.bom")
 }
+
+micronautBuild {
+    //TODO remove when initial release is done
+    binaryCompatibility.enabled.set(false)
+}
+
+//TODO remove when micronaut-build 5.3.15 is out
+tasks.checkVersionCatalogCompatibility.configure { enabled = false }
