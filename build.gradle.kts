f2427ca3c8d94f4aefd0be0def0112ef31222fed
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2022-08-03 11:15:48 +0200

    Fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,10 @@ plugins {
     io.micronaut.build.internal.docs
     io.micronaut.build.internal.`quality-reporting`
 }
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+    }
+}
