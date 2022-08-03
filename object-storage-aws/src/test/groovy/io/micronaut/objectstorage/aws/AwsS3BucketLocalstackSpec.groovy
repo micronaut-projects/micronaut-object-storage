c63c7c6eacee6fa7592e08bbe7c20e142e9830c4
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2022-08-03 14:17:30 +0200

    Refactor tests to avoid copy/paste

--- object-storage-aws/src/test/groovy/io/micronaut/objectstorage/aws/AwsS3BucketLocalstackSpec.groovy
+++ object-storage-aws/src/test/groovy/io/micronaut/objectstorage/aws/AwsS3BucketLocalstackSpec.groovy
@@ -1,62 +1,31 @@
 package io.micronaut.objectstorage.aws
 
-import io.micronaut.context.BeanProvider
-import io.micronaut.objectstorage.ObjectStorage
-import io.micronaut.objectstorage.ObjectStorageSpecification
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import io.micronaut.test.support.TestPropertyProvider
-import jakarta.inject.Inject
-import jakarta.inject.Named
 import org.testcontainers.containers.localstack.LocalStackContainer
 import org.testcontainers.utility.DockerImageName
-import software.amazon.awssdk.services.s3.S3Client
-import software.amazon.awssdk.services.s3.model.CreateBucketRequest
-import software.amazon.awssdk.services.s3.model.DeleteBucketRequest
 import spock.lang.AutoCleanup
 import spock.lang.Shared
 
 import static org.testcontainers.containers.localstack.LocalStackContainer.Service.S3
 
 @MicronautTest
-class AwsS3BucketLocalstackSpec extends ObjectStorageSpecification implements TestPropertyProvider {
-
-    private static final String BUCKET_NAME = "test-bucket"
+class AwsS3BucketLocalstackSpec extends AwsSpec implements TestPropertyProvider {
 
     @Shared
     @AutoCleanup
     public LocalStackContainer localstack = new LocalStackContainer(DockerImageName.parse("localstack/localstack:1.0.3"))
             .withServices(S3)
 
-    @Named("default")
-    @Inject
-    BeanProvider<AwsS3Bucket> awsS3Bucket
-
-    @Inject
-    S3Client s3
-
     @Override
     Map<String, String> getProperties() {
         localstack.start()
-        return [
-                (AwsS3BucketConfiguration.PREFIX + ".default.name"): BUCKET_NAME,
+        return super.getProperties() + [
                 "aws.accessKeyId": localstack.getAccessKey(),
                 "aws.secretKey": localstack.getSecretKey(),
                 "aws.region": localstack.getRegion(),
                 "aws.s3.endpoint-override": localstack.getEndpointOverride(S3)
 
         ]
-    }
-
-    void setup() {
-        s3.createBucket(CreateBucketRequest.builder().bucket(BUCKET_NAME).build() as CreateBucketRequest)
-    }
-
-    void cleanup() {
-        s3.deleteBucket(DeleteBucketRequest.builder().bucket(BUCKET_NAME).build() as DeleteBucketRequest)
-    }
-
-    @Override
-    ObjectStorage getObjectStorage() {
-        return awsS3Bucket.get()
     }
 }
