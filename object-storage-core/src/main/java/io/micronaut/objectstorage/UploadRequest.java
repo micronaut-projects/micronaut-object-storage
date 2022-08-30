a0bbcd1a52130031bf4f790189a6aa51aa79db71
Sergio del Amo <sergio.delamo@softamo.com>
2022-08-30 17:31:05 +0200

    add nullability annotations to UploadRequest

--- object-storage-core/src/main/java/io/micronaut/objectstorage/UploadRequest.java
+++ object-storage-core/src/main/java/io/micronaut/objectstorage/UploadRequest.java
@@ -17,6 +17,8 @@ package io.micronaut.objectstorage;
 
 import io.micronaut.core.annotation.NonNull;
 import io.micronaut.core.annotation.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -32,14 +34,17 @@ import java.util.Optional;
  */
 public interface UploadRequest {
 
+    @NonNull
     static UploadRequest fromPath(@NonNull Path path) {
         return new FileUploadRequest(path);
     }
 
+    @NonNull
     static UploadRequest fromPath(@NonNull Path path, String prefix) {
         return new FileUploadRequest(path, prefix);
     }
 
+    @NonNull
     static UploadRequest fromBytes(@NonNull byte[] bytes,
                                    @NonNull String contentType,
                                    @NonNull String key) {
@@ -49,21 +54,25 @@ public interface UploadRequest {
     /**
      * @return the content type of this upload request.
      */
+    @NonNull
     Optional<String> getContentType();
 
     /**
      * @return the file name with path.
      */
+    @NonNull
     String getKey();
 
     /**
      * @return the size of the part, in bytes.
      */
+    @NonNull
     Optional<Long> getContentSize();
 
     /**
      * @return an input stream of the object to be stored.
      */
+    @NonNull
     InputStream getInputStream();
 
     /**
@@ -71,22 +80,25 @@ public interface UploadRequest {
      */
     class FileUploadRequest implements UploadRequest {
 
+        private static final Logger LOG = LoggerFactory.getLogger(FileUploadRequest.class);
+
         private final String keyName;
         private final String contentType;
         private final Path path;
 
-        public FileUploadRequest(Path localFilePath) {
+        public FileUploadRequest(@NonNull Path localFilePath) {
             this(localFilePath, localFilePath.getFileName().toString(), null,
                 URLConnection.guessContentTypeFromName(localFilePath.toFile().getName()));
         }
 
-        public FileUploadRequest(Path localFilePath, String prefix) {
+        public FileUploadRequest(@NonNull Path localFilePath,
+                                 @Nullable String prefix) {
             this(localFilePath, localFilePath.toFile().getName(), prefix,
                 URLConnection.guessContentTypeFromName(localFilePath.toFile().getName()));
         }
 
-        public FileUploadRequest(Path localFilePath,
-                                 String keyName,
+        public FileUploadRequest(@NonNull Path localFilePath,
+                                 @NonNull String keyName,
                                  @Nullable String prefix,
                                  @Nullable String contentType) {
             this.keyName = prefix != null ? prefix + "/" + keyName : keyName;
@@ -94,42 +106,55 @@ public interface UploadRequest {
             this.path = localFilePath;
         }
 
+        @NonNull
         public File getFile() {
             return path.toFile();
         }
 
+        @NonNull
         public Path getPath() {
             return path;
         }
 
+        @NonNull
         public String getAbsolutePath() {
             return path.toAbsolutePath().toString();
         }
 
         @Override
+        @NonNull
         public Optional<String> getContentType() {
             return Optional.ofNullable(contentType);
         }
 
         @Override
+        @NonNull
         public String getKey() {
             return keyName;
         }
 
         @Override
+        @NonNull
         public Optional<Long> getContentSize() {
             try {
                 return Optional.of(Files.size(path));
             } catch (IOException e) {
+                if (LOG.isErrorEnabled()) {
+                    LOG.error("Error calculating the size of the file.", e);
+                }
                 return Optional.empty();
             }
         }
 
         @Override
+        @NonNull
         public InputStream getInputStream() {
             try {
                 return Files.newInputStream(path);
             } catch (IOException e) {
+                if (LOG.isErrorEnabled()) {
+                    LOG.error("Error creating an input stream to read from the file.", e);
+                }
                 throw new ObjectStorageException(e);
             }
         }
@@ -140,8 +165,13 @@ public interface UploadRequest {
      */
     class BytesUploadRequest implements UploadRequest {
 
+        @NonNull
         private final byte[] bytes;
+
+        @NonNull
         private final String contentType;
+
+        @NonNull
         private final String key;
 
         public BytesUploadRequest(@NonNull byte[] bytes,
@@ -153,21 +183,25 @@ public interface UploadRequest {
         }
 
         @Override
+        @NonNull
         public Optional<String> getContentType() {
             return Optional.of(contentType);
         }
 
         @Override
+        @NonNull
         public String getKey() {
             return key;
         }
 
         @Override
+        @NonNull
         public Optional<Long> getContentSize() {
             return Optional.of((long) bytes.length);
         }
 
         @Override
+        @NonNull
         public InputStream getInputStream() {
             return new ByteArrayInputStream(bytes);
         }
