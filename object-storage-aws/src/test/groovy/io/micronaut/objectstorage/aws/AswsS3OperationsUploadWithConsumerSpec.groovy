cd7f4f489aebd6d144c35fb1cf8d7daa6b641bdc
Sergio del Amo <sergio.delamo@softamo.com>
2022-09-01 08:29:43 +0200

    add test for consumer

---
+++ object-storage-aws/src/test/groovy/io/micronaut/objectstorage/aws/AswsS3OperationsUploadWithConsumerSpec.groovy
@@ -0,0 +1,72 @@
+package io.micronaut.objectstorage.aws
+
+import io.micronaut.context.annotation.Property
+import io.micronaut.context.annotation.Replaces
+import io.micronaut.context.annotation.Requires
+import io.micronaut.objectstorage.ObjectStorageOperations
+import io.micronaut.objectstorage.UploadRequest;
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import jakarta.inject.Inject
+import jakarta.inject.Singleton
+import software.amazon.awssdk.awscore.exception.AwsServiceException
+import software.amazon.awssdk.core.exception.SdkClientException
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL
+import software.amazon.awssdk.services.s3.model.PutObjectRequest
+import software.amazon.awssdk.services.s3.model.PutObjectResponse
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import spock.lang.Specification
+
+import java.nio.file.Files
+import java.nio.file.Path;
+
+@Property(name = "micronaut.object-storage.aws.default.bucket", value = "profile-pictures-bucket")
+@Property(name = "spec.name", value = "AswsS3OperationsUploadWithConsumerSpec")
+@MicronautTest
+class AwsS3OperationsUploadWithConsumerSpec extends Specification {
+
+    @Inject
+    ObjectStorageOperations<PutObjectRequest.Builder, PutObjectResponse> objectStorage
+
+    @Inject
+    S3ClientReplacement s3ClientReplacement
+
+    void "consumer accept is invoked"() {
+        given:
+        Path tempFilePath = Files.createTempFile('test-file', 'txt')
+        tempFilePath.toFile().text = 'micronaut'
+        UploadRequest uploadRequest = UploadRequest.fromPath(tempFilePath)
+
+        when:
+        objectStorage.upload(uploadRequest, { builder ->
+            builder.acl(ObjectCannedACL.PUBLIC_READ)
+        })
+
+        then:
+        s3ClientReplacement.request
+        ObjectCannedACL.PUBLIC_READ == s3ClientReplacement.request.acl()
+    }
+
+    @Requires(property = "spec.name", value = "AswsS3OperationsUploadWithConsumerSpec")
+    @Replaces(S3Client)
+    @Singleton
+    static class S3ClientReplacement implements S3Client {
+        PutObjectRequest request
+
+        @Override
+        PutObjectResponse putObject(PutObjectRequest putObjectRequest, Path sourcePath) throws AwsServiceException,
+                SdkClientException, S3Exception {
+            this.request = putObjectRequest
+            null
+        }
+
+        @Override
+        String serviceName() {
+            's3-client-replacement'
+        }
+        @Override
+        void close() {
+
+        }
+    }
+}
