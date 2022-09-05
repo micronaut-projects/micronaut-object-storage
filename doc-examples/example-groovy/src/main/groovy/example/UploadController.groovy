e7c27ad41ffd3c1dd9a3767a606a75b9e3140080
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2022-09-05 17:46:39 +0200

    Added Groovy and Kotlin samples

---
+++ doc-examples/example-groovy/src/main/groovy/example/UploadController.groovy
@@ -0,0 +1,35 @@
+package example
+
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Post
+import io.micronaut.http.multipart.CompletedFileUpload
+import io.micronaut.objectstorage.aws.AwsS3Operations
+import io.micronaut.objectstorage.request.UploadRequest
+import io.micronaut.objectstorage.response.UploadResponse
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL
+import software.amazon.awssdk.services.s3.model.PutObjectResponse
+
+@Controller
+class UploadController {
+
+    final AwsS3Operations objectStorage
+
+    UploadController(AwsS3Operations objectStorage) {
+        this.objectStorage = objectStorage
+    }
+
+    @Post(consumes = MediaType.MULTIPART_FORM_DATA, produces = MediaType.TEXT_PLAIN)
+    HttpResponse<String> upload(CompletedFileUpload fileUpload) {
+        UploadRequest objectStorageUpload = UploadRequest.fromCompletedFileUpload(fileUpload)
+        UploadResponse<PutObjectResponse> response = objectStorage.upload(objectStorageUpload, { builder ->
+            builder.acl(ObjectCannedACL.PUBLIC_READ)
+        })
+
+        return HttpResponse
+                .created(response.key)
+                .header("ETag", response.getNativeResponse().eTag())
+    }
+
+}
---
+++ doc-examples/example-kotlin/src/main/kotlin/example/UploadController.kt
@@ -0,0 +1,28 @@
+package example
+
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Post
+import io.micronaut.http.multipart.CompletedFileUpload
+import io.micronaut.objectstorage.aws.AwsS3Operations
+import io.micronaut.objectstorage.request.UploadRequest
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL
+import software.amazon.awssdk.services.s3.model.PutObjectRequest
+
+@Controller
+open class UploadController(private val objectStorage: AwsS3Operations) {
+
+    @Post(consumes = [MediaType.MULTIPART_FORM_DATA], produces = [MediaType.TEXT_PLAIN])
+    open fun upload(fileUpload: CompletedFileUpload): HttpResponse<String>? {
+        val objectStorageUpload = UploadRequest.fromCompletedFileUpload(fileUpload)
+        val response = objectStorage.upload(objectStorageUpload) { builder: PutObjectRequest.Builder ->
+            builder.acl(ObjectCannedACL.PUBLIC_READ)
+        }
+
+        return HttpResponse
+            .created(response.key)
+            .header("ETag", response.nativeResponse.eTag())
+    }
+
+}
