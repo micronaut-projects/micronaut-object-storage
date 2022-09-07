4983c3098921b5aff5fe3cb4a570f246f47629d2
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2022-09-07 15:14:52 +0200

    Try fix test

--- doc-examples/example-java/src/test/groovy/example/StorageOptionsBuilderCustomizerSpec.groovy
+++ doc-examples/example-java/src/test/groovy/example/StorageOptionsBuilderCustomizerSpec.groovy
@@ -1,14 +1,30 @@
 package example
 
+import com.google.auth.oauth2.GoogleCredentials
 import com.google.cloud.storage.StorageOptions
 import io.micronaut.context.ApplicationContext
+import io.micronaut.context.annotation.Factory
+import io.micronaut.context.annotation.Primary
+import io.micronaut.context.annotation.Requires
+import jakarta.inject.Singleton
+import spock.lang.Shared
 import spock.lang.Specification
 
 class StorageOptionsBuilderCustomizerSpec extends Specification {
 
+    public static final String SPEC_NAME = 'StorageOptionsBuilderCustomizerSpec'
+
+    @Shared
+    static GoogleCredentials credentialsMock
+
     void "it can customize the GCP client"() {
         given:
-        ApplicationContext ctx = ApplicationContext.run("gcp.project-id": "test-project")
+        credentialsMock = Mock(GoogleCredentials)
+        ApplicationContext ctx = ApplicationContext.run(
+                "gcp.project-id": "test-project-basdasdasd",
+                "gcp.credentials.enabled": false,
+                "spec.name": SPEC_NAME
+        )
 
         when:
         StorageOptions client = ctx.getBean(StorageOptions)
@@ -18,6 +34,18 @@ class StorageOptionsBuilderCustomizerSpec extends Specification {
 
         cleanup:
         ctx.close()
+    }
+
+    @Factory
+    @Requires(property = 'spec.name', value = SPEC_NAME)
+    static class TestCredentialsFactory {
+
+        @Singleton
+        @Primary
+        GoogleCredentials googleCredentials() {
+            credentialsMock
+        }
+
     }
 
 }
