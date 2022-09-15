f019f16f4608f2b8a9ad4cf98957485254050f89
Sergio del Amo <sergio.delamo@softamo.com>
2022-09-15 16:51:05 +0200

    enable binary compatibility for BOM (#120)

--- object-storage-bom/build.gradle.kts
+++ object-storage-bom/build.gradle.kts
@@ -1,11 +1,3 @@
 plugins {
     io.micronaut.build.internal.bom
 }
-
-micronautBuild {
-    //TODO remove when initial release is done
-    binaryCompatibility.enabled.set(false)
-}
-
-//TODO remove when micronaut-build 5.3.15 is out
-tasks.checkVersionCatalogCompatibility.configure { enabled = false }
