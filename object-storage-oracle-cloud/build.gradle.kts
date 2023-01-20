86d5a53f8e583f1c5a92b3f133a6349f19fbfc47
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2023-01-20 12:16:41 +0100

    Refreshing versions. Awaitility needs to be removed though

--- object-storage-oracle-cloud/build.gradle.kts
+++ object-storage-oracle-cloud/build.gradle.kts
@@ -4,7 +4,9 @@ plugins {
 
 dependencies {
     api(projects.objectStorageCore)
-    api(mnOraclecloud.micronaut.oraclecloud.sdk)
+    api(mnOraclecloud.micronaut.oraclecloud.sdk) {
+        exclude(group = "org.codehaus.groovy", module = "groovy")
+    }
     api(libs.oci.sdk.objectstorage)
 
     implementation(platform(mnOraclecloud.micronaut.oraclecloud.bom))
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 }
 
 plugins {
-    id("io.micronaut.build.shared.settings") version "6.1.1"
+    id("io.micronaut.build.shared.settings") version "6.2.0"
 }
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
