e5918bc749a51c5beccfdd3e34b6c91d9796a8b3
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2023-01-23 16:07:38 +0100

    Re-enable Micronaut Oracle Cloud Netty Client

--- object-storage-oracle-cloud/build.gradle.kts
+++ object-storage-oracle-cloud/build.gradle.kts
@@ -6,9 +6,7 @@ dependencies {
     api(projects.objectStorageCore)
     api(mnOraclecloud.micronaut.oraclecloud.sdk) {
         exclude(group = "org.codehaus.groovy", module = "groovy")
-        exclude(group = "io.micronaut.oraclecloud", module = "micronaut-oraclecloud-httpclient-netty")
     }
-    api(libs.oci.sdk.httpclient.jersey3)
     api(libs.oci.sdk.objectstorage)
 
     implementation(platform(mnOraclecloud.micronaut.oraclecloud.bom))
