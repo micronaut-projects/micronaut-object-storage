6cbb3345b19f3a3b561e2e1da8a8136f415eccc7
Sergio del Amo <sergio.delamo@softamo.com>
2023-06-04 17:50:50 +0200

    PendingFeature for Azure client customization test
    readTimeout is no longer exposed

--- doc-examples/example-java/src/test/groovy/example/BlobServiceClientBuilderCustomizerSpec.groovy
+++ doc-examples/example-java/src/test/groovy/example/BlobServiceClientBuilderCustomizerSpec.groovy
@@ -2,12 +2,14 @@ package example
 
 import com.azure.storage.blob.BlobServiceClient
 import io.micronaut.context.ApplicationContext
+import spock.lang.PendingFeature
 import spock.lang.Specification
 
 import static io.micronaut.objectstorage.azure.AzureBlobStorageConfiguration.PREFIX
 
 class BlobServiceClientBuilderCustomizerSpec extends Specification {
 
+    @PendingFeature(reason = "readTimeout No longer exposed")
     void "it can customize the Azure client"() {
         given:
         ApplicationContext ctx = ApplicationContext.run((PREFIX + '.default.endpoint'): "https://127.0.0.1:10000/devstoreaccount1")
