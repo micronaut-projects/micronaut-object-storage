f4903d34ef05dc3516bdb83aa2e6e02e7266671e
Sergio del Amo <sergio.delamo@softamo.com>
2023-10-12 16:42:48 +0200

    test: only create gcp bucket once

--- object-storage-gcp/src/test/groovy/io/micronaut/objectstorage/googlecloud/AbstractGoogleCloudStorageSpec.groovy
+++ object-storage-gcp/src/test/groovy/io/micronaut/objectstorage/googlecloud/AbstractGoogleCloudStorageSpec.groovy
@@ -8,6 +8,7 @@ import io.micronaut.objectstorage.ObjectStorageOperations
 import io.micronaut.objectstorage.ObjectStorageOperationsSpecification
 import io.micronaut.test.support.TestPropertyProvider
 import jakarta.inject.Inject
+import spock.lang.Shared
 
 import static io.micronaut.objectstorage.googlecloud.GoogleCloudStorageConfiguration.PREFIX
 
@@ -19,6 +20,10 @@ abstract class AbstractGoogleCloudStorageSpec extends ObjectStorageOperationsSpe
     @Inject
     GoogleCloudStorageOperations cloudObjectStorage
 
+    @Shared
+    boolean bucketCreated
+
+    @Shared
     @Inject
     Storage storage
 
@@ -32,10 +37,14 @@ abstract class AbstractGoogleCloudStorageSpec extends ObjectStorageOperationsSpe
     }
 
     void setup() {
-        storage.create(BucketInfo.newBuilder(BUCKET_NAME).build());
+        if (!bucketCreated) {
+            storage.create(BucketInfo.newBuilder(BUCKET_NAME).build())
+            bucketCreated = true
+        }
+
     }
 
-    void cleanup() {
+    void cleanupSpec() {
         storage.get(BUCKET_NAME).delete()
     }
 }
