e3c6e31c164d6bf4aa9eb11ca592734d293ef9d7
Sergio del Amo <sergio.delamo@softamo.com>
2024-08-21 17:16:40 +0200

    Object Storage to 4.6.x versions (#516)
    * fix(deps): update dependency io.micronaut.oraclecloud:micronaut-oraclecloud-bom to v4
    * fix(deps): update dependency io.micronaut:micronaut-core-bom to v4.6.1
    * fix(deps): update dependency io.micronaut.azure:micronaut-azure-bom to v5.6.0
    * fix(deps): update dependency io.micronaut.gcp:micronaut-gcp-bom to v5.7.0
    * fix(deps): update dependency io.micronaut.gradle:micronaut-gradle-plugin to v4.4.2
    * fix(deps): update dependency io.micronaut.platform:micronaut-platform to v4.5.1	gradle/libs.versions.toml
    * fix(deps): update dependency io.micronaut.testresources:micronaut-test-resources-bom to v2.5.4
    * test 4.5.0
    * aws 4.7.0
    * validation 4.7.0
    * logging 1.4.0
    * update azurite container
    * kotlin 1.9.25

--- object-storage-azure/src/test/groovy/io/micronaut/objectstorage/azure/AzureBlobStorageAzuriteSpec.groovy
+++ object-storage-azure/src/test/groovy/io/micronaut/objectstorage/azure/AzureBlobStorageAzuriteSpec.groovy
@@ -15,7 +15,7 @@ class AzureBlobStorageAzuriteSpec extends AbstractAzureBlobStorageSpec {
 
     @Shared
     @AutoCleanup
-    GenericContainer azuriteContainer = new GenericContainer(DockerImageName.parse('mcr.microsoft.com/azure-storage/azurite:3.28.0'))
+    GenericContainer azuriteContainer = new GenericContainer(DockerImageName.parse('mcr.microsoft.com/azure-storage/azurite:3.32.0'))
             .withExposedPorts(10000)
 
     @Override
