a5aaf297ab35bfe5c03625314828eb08af2d2463
issam liqali <liqaliissam@gmail.com>
2025-10-29 10:49:37 +0100

    remove unused java

--- test-suite-utils/build.gradle.kts
+++ test-suite-utils/build.gradle.kts
@@ -2,7 +2,3 @@ plugins {
     java
     id ("io.micronaut.build.internal.java-base")
 }
-java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
-}
