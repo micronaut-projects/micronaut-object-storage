e469b54ef31a6a5163733d6706863c191d5fade2
Sergio del Amo <sergio.delamo@softamo.com>
2026-02-27 14:32:08 +0100

    enable oracle cloud module in object storage (#705)
    * Revert "remove oracle cloud docs"
    
    This reverts commit 68e4df43a9296cc7b65e0e0a37644766b970ec73.
    
    * Revert "disable oracle cloud support"
    
    This reverts commit 0d7788dbacb11cab2ba70cae968aabde6ba00390.
    
    * Update micronaut-oracle-cloud version to 6.0.0-M1
    
    * Update Micronaut version to 5.0.0-M14

---
+++ doc-examples/example-java/src/main/java/example/oraclecloud/ObjectStorageClientBuilderCustomizer.java
@@ -0,0 +1,31 @@
+package example.oraclecloud;
+
+import com.oracle.bmc.ClientConfiguration;
+import com.oracle.bmc.objectstorage.ObjectStorageClient;
+import io.micronaut.context.event.BeanCreatedEvent;
+import io.micronaut.context.event.BeanCreatedEventListener;
+import org.jspecify.annotations.NonNull;
+import jakarta.inject.Singleton;
+
+//tag::class[]
+//See https://github.com/oracle/oci-java-sdk/blob/master/bmc-examples/src/main/java/ClientConfigurationTimeoutExample.java
+@Singleton
+public class ObjectStorageClientBuilderCustomizer implements BeanCreatedEventListener<ObjectStorageClient.Builder> {
+
+    public static final int CONNECTION_TIMEOUT_IN_MILLISECONDS = 25000;
+    public static final int READ_TIMEOUT_IN_MILLISECONDS = 35000;
+
+    @Override
+    public ObjectStorageClient.Builder onCreated(@NonNull BeanCreatedEvent<ObjectStorageClient.Builder> event) {
+        ClientConfiguration clientConfiguration =
+            ClientConfiguration.builder()
+                .connectionTimeoutMillis(CONNECTION_TIMEOUT_IN_MILLISECONDS)
+                .readTimeoutMillis(READ_TIMEOUT_IN_MILLISECONDS)
+                .build();
+
+
+        return event.getBean()
+            .configuration(clientConfiguration);
+    }
+}
+//end::class[]
