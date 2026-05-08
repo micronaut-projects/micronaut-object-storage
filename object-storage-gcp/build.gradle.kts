da17e0c1c73dd4355359bd0b4ccf0c9306172004
Sergio del Amo <sergio.delamo@softamo.com>
2026-05-08 16:32:25 +0200

    force jackson to a non vulnerable version

--- object-storage-gcp/build.gradle.kts
+++ object-storage-gcp/build.gradle.kts
@@ -7,8 +7,11 @@ dependencies {
     api(mnGcp.micronaut.gcp.common)
 
     api(platform(libs.gcp.libraries.bom))
-    api(libs.gcp.storage)
-
+    api(libs.gcp.storage) {
+        exclude(group = "com.fasterxml.jackson.core", module = "jackson-core")
+    }
+    // "com.google.cloud:google-cloud-storage 26.80.0 requests jackson-core 2.18.1, which is affected by GHSA-72hv-8253-57qq"
+    api("com.fasterxml.jackson.core:jackson-core:2.18.6")
     implementation(platform(mnGcp.micronaut.gcp.bom))
 
     testImplementation(mnValidation.micronaut.validation)
