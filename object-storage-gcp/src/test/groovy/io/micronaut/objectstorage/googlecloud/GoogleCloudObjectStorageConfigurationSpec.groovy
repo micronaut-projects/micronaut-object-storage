57364ff94a83c225755994f43d5f571caa02b4e5
Sergio del Amo <sergio.del.amo.caballero@oracle.com>
2026-07-21 10:37:07 +0200

    Test named GCP reactive storage operations
    Add coverage for multiple Google Cloud object storage configurations to verify that named ReactiveObjectStorageOperations beans are created for each configured bucket.

--- object-storage-gcp/src/test/groovy/io/micronaut/objectstorage/googlecloud/GoogleCloudObjectStorageConfigurationSpec.groovy
+++ object-storage-gcp/src/test/groovy/io/micronaut/objectstorage/googlecloud/GoogleCloudObjectStorageConfigurationSpec.groovy
@@ -1,6 +1,11 @@
 package io.micronaut.objectstorage.googlecloud
 
+import com.google.cloud.storage.Storage
+import io.micronaut.context.ApplicationContext
+import io.micronaut.context.env.Environment
 import io.micronaut.objectstorage.ObjectStorageConfigurationSpecification
+import io.micronaut.objectstorage.ReactiveObjectStorageOperations
+import io.micronaut.inject.qualifiers.Qualifiers
 
 class GoogleCloudObjectStorageConfigurationSpec extends ObjectStorageConfigurationSpecification<GoogleCloudStorageOperations> {
 
@@ -12,5 +17,25 @@ class GoogleCloudObjectStorageConfigurationSpec extends ObjectStorageConfigurati
     @Override
     Class<GoogleCloudStorageOperations> getObjectStorage() {
         return GoogleCloudStorageOperations.class
+    }
+
+    void "creates named reactive operations for each GCP storage configuration"() {
+        given:
+        def ctx = ApplicationContext.run([
+                (configPrefix + '.default.bucket'): 'default-bucket',
+                (configPrefix + '.other.bucket')  : 'other-bucket'
+        ], Environment.TEST)
+        ctx.registerSingleton(Storage, Mock(Storage))
+
+        when:
+        def defaultOperations = ctx.getBean(ReactiveObjectStorageOperations, Qualifiers.byName('default'))
+        def otherOperations = ctx.getBean(ReactiveObjectStorageOperations, Qualifiers.byName('other'))
+
+        then:
+        defaultOperations
+        otherOperations
+
+        cleanup:
+        ctx.close()
     }
 }
